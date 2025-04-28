403d80b251e7c5ec52587b56f5db35213656499f
Eduardo <eduardoca5@al.insper.edu.br>
2025-04-28 14:53:48 -0300

    Questao 3

--- funcoes.py
+++ funcoes.py
@@ -18,3 +18,13 @@ def guardar_dado(lista_dados, dados_guardados, numero_inteiro):
             dados_guardados.append(lista_dados[i]) 
         i +=1             
     return [nova_lista, dados_guardados] 
+
+def remover_dado (dados_rolados,dados_guardados,numero_inteiro):
+    nova_lista = []
+    i = 0 
+    while i < len(dados_guardados):
+        if dados_guardados[i]==numero_inteiro:
+            dados_rolados.append(dados_guardados[i])
+        else:
+            nova_lista.append(dados_guardados[i])
+    return [dados_rolados,nova_lista]
