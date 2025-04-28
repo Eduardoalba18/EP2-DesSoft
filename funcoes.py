4a7cb9b30a7f0836e372d9e683c158899433513b
Eduardo <eduardoca5@al.insper.edu.br>
2025-04-28 11:31:56 -0300

    Questao 2

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,14 @@ def rolar_dados (numero_inteiro):
         lista_dados.append(numero)
         i += 1
     return lista_dados
+
+def guardar_dado(lista_dados, dados_guardados, numero_inteiro):
+    i = 0
+    nova_lista = []
+    while i < len(lista_dados):
+        if i != numero_inteiro:
+            nova_lista.append(lista_dados[i])
+        else:
+            dados_guardados.append(lista_dados[i]) 
+        i +=1             
+    return [lista_dados, dados_guardados] 
